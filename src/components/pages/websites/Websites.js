0865c7041abf0b961be637adeae2ecda6725b273
patriciadgao <patriciadgao@gmail.com>
2024-05-28 23:25:50 -0600

    width thing

--- src/components/pages/websites/Websites.js
+++ src/components/pages/websites/Websites.js
@@ -9,15 +9,17 @@ export function Websites() {
         Here is a collection of websites and other projects I've made. It's kind of small for now,
         but that's because I've been busy coding up this one :)
       </Paragraph>
-      <div className="flex gap-4">
-        <Card
-          pic={require("../../../img/websites/flowersite.png")}
-          link="https://patriciadgao.github.io/dichotomouskey"
-          title="Pat's Flowers"
-        >
-          This site is full of flowers I've photographed and identified. I
-          update it pretty regularly when I see new flowers.
-        </Card>
+      <div className="flex gap-4 justify-center">
+        <div className="max-w-[500px]">
+          <Card
+            pic={require("../../../img/websites/flowersite.png")}
+            link="https://patriciadgao.github.io/dichotomouskey"
+            title="Pat's Flowers"
+          >
+            This site is full of flowers I've photographed and identified. I
+            update it pretty regularly when I see new flowers.
+          </Card>
+        </div>
         {/* <Card
           navigateTo="patsquestions"
           title="Pat's Questions"
